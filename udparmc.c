6f3680fe5fc75ab4977779bc20ccea1cd0127986
Samuel B <151054011+samuel-404@users.noreply.github.com>
2026-05-20 20:40:18 +0530

    Create udparmc.c

---
+++ udparmc.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main()
+{
+    int clientsocket, port;
+    int num;
+
+    char result[50];
+
+    struct sockaddr_in serveraddr;
+    socklen_t len;
+
+    clientsocket = socket(AF_INET, SOCK_DGRAM, 0);
+
+    memset(&serveraddr, 0, sizeof(serveraddr));
+
+    printf("Enter Port Number: ");
+    scanf("%d", &port);
+
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(port);
+    serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    printf("Enter Number: ");
+    scanf("%d", &num);
+
+    sendto(clientsocket,
+           &num,
+           sizeof(num),
+           0,
+           (struct sockaddr*)&serveraddr,
+           sizeof(serveraddr));
+
+    len = sizeof(serveraddr);
+
+    recvfrom(clientsocket,
+             result,
+             sizeof(result),
+             0,
+             (struct sockaddr*)&serveraddr,
+             &len);
+
+    printf("%s\n", result);
+
+    close(clientsocket);
+
+    return 0;
+}
